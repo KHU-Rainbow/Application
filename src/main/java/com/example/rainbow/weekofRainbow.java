48efba0eec295a2217dc9d0feb62cb3dd3be1b7b
jina <2017101614@khu.ac.kr>
2020-10-25 13:11:45 +0900

    jina : 1026 weekofRainbow 크기 조정

--- src/main/java/com/example/rainbow/weekofRainbow.java
+++ src/main/java/com/example/rainbow/weekofRainbow.java
@@ -1,8 +1,10 @@
 package com.example.rainbow;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /* textView id : week_1, week_2...  ImageView id : week_pic_1, week_pic_2....
 무지개 : rb_pic7
@@ -13,6 +15,7 @@ import android.widget.ImageView;
 */
 public class weekofRainbow extends Activity {
     private ImageView week_rbpic1, week_rbpic2, week_rbpic3, week_rbpic4, week_rbpic5;
+    private TextView week1, week2, week3, week4, week5;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -29,6 +32,18 @@ public class weekofRainbow extends Activity {
         week_rbpic3.setImageResource(R.drawable.rain_pic5);
         week_rbpic4.setImageResource(R.drawable.rb_pic7);
         week_rbpic5.setImageResource(R.drawable.rb_pic8);
+
+        week1 = (TextView) findViewById(R.id.week_1);
+        week2 = (TextView) findViewById(R.id.week_2);
+        week3 = (TextView) findViewById(R.id.week_3);
+        week4 = (TextView) findViewById(R.id.week_4);
+        week5 = (TextView) findViewById(R.id.week_5);
+        // 빨 - 주 - 초 - 파 - 보
+        week1.setTextColor(Color.parseColor("#eb4934"));
+        week2.setTextColor(Color.parseColor("#eba834"));
+        week3.setTextColor(Color.parseColor("#3bba14"));
+        week4.setTextColor(Color.parseColor("#1288cc"));
+        week5.setTextColor(Color.parseColor("#a753f5"));
 
     }
 
