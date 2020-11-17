0d5a7181902299c2cc57fee0fdfef9b75c7a9e85
sehee <sea11hee@gmail.com>
2020-11-17 22:46:35 +0900

    sehee: [추가] Retrofit 사용을 위한 권한 및 클래스 추가

---
+++ src/main/java/com/example/rainbow/PostItem.java
@@ -0,0 +1,20 @@
+package com.example.rainbow;
+
+public class PostItem {
+
+
+    private String date;
+    private int time;
+    private int goal;
+    private int detectTime;
+
+    // achievement가 1인가 0인가
+    private int IsAchieved;
+
+    private String[] achievementList;
+    private int[] estimation;
+
+
+
+
+}
